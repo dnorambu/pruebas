bb02c25146db950ecec8b69bb639486311993c6a
dnorambu <diego.norambuena@sansano.usm.cl>
2020-10-19 23:19:56 -0300

    Subo camion

--- courier/courier_grpc.pb.go
+++ courier/courier_grpc.pb.go
@@ -20,6 +20,10 @@ type CourierServiceClient interface {
 	EnviarOrdenPyme(ctx context.Context, in *OrdenPyme, opts ...grpc.CallOption) (*Codigo, error)
 	EnviarOrdenRetail(ctx context.Context, in *OrdenRetail, opts ...grpc.CallOption) (*Empty, error)
 	Seguimiento(ctx context.Context, in *Codigo, opts ...grpc.CallOption) (*Mensaje, error)
+	PedirRetail(ctx context.Context, in *Mensaje, opts ...grpc.CallOption) (*Paquete, error)
+	PedirPrioritario(ctx context.Context, in *Mensaje, opts ...grpc.CallOption) (*Paquete, error)
+	PedirNormal(ctx context.Context, in *Mensaje, opts ...grpc.CallOption) (*Paquete, error)
+	ResultadoEntrega(ctx context.Context, in *Entrega, opts ...grpc.CallOption) (*Mensaje, error)
 }
 
 type courierServiceClient struct {
@@ -57,6 +61,42 @@ func (c *courierServiceClient) Seguimiento(ctx context.Context, in *Codigo, opts
 	return out, nil
 }
 
+func (c *courierServiceClient) PedirRetail(ctx context.Context, in *Mensaje, opts ...grpc.CallOption) (*Paquete, error) {
+	out := new(Paquete)
+	err := c.cc.Invoke(ctx, "/CourierService/PedirRetail", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *courierServiceClient) PedirPrioritario(ctx context.Context, in *Mensaje, opts ...grpc.CallOption) (*Paquete, error) {
+	out := new(Paquete)
+	err := c.cc.Invoke(ctx, "/CourierService/PedirPrioritario", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *courierServiceClient) PedirNormal(ctx context.Context, in *Mensaje, opts ...grpc.CallOption) (*Paquete, error) {
+	out := new(Paquete)
+	err := c.cc.Invoke(ctx, "/CourierService/PedirNormal", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *courierServiceClient) ResultadoEntrega(ctx context.Context, in *Entrega, opts ...grpc.CallOption) (*Mensaje, error) {
+	out := new(Mensaje)
+	err := c.cc.Invoke(ctx, "/CourierService/ResultadoEntrega", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // CourierServiceServer is the server API for CourierService service.
 // All implementations must embed UnimplementedCourierServiceServer
 // for forward compatibility
@@ -64,6 +104,10 @@ type CourierServiceServer interface {
 	EnviarOrdenPyme(context.Context, *OrdenPyme) (*Codigo, error)
 	EnviarOrdenRetail(context.Context, *OrdenRetail) (*Empty, error)
 	Seguimiento(context.Context, *Codigo) (*Mensaje, error)
+	PedirRetail(context.Context, *Mensaje) (*Paquete, error)
+	PedirPrioritario(context.Context, *Mensaje) (*Paquete, error)
+	PedirNormal(context.Context, *Mensaje) (*Paquete, error)
+	ResultadoEntrega(context.Context, *Entrega) (*Mensaje, error)
 	mustEmbedUnimplementedCourierServiceServer()
 }
 
@@ -79,6 +123,18 @@ func (UnimplementedCourierServiceServer) EnviarOrdenRetail(context.Context, *Ord
 }
 func (UnimplementedCourierServiceServer) Seguimiento(context.Context, *Codigo) (*Mensaje, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Seguimiento not implemented")
+}
+func (UnimplementedCourierServiceServer) PedirRetail(context.Context, *Mensaje) (*Paquete, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method PedirRetail not implemented")
+}
+func (UnimplementedCourierServiceServer) PedirPrioritario(context.Context, *Mensaje) (*Paquete, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method PedirPrioritario not implemented")
+}
+func (UnimplementedCourierServiceServer) PedirNormal(context.Context, *Mensaje) (*Paquete, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method PedirNormal not implemented")
+}
+func (UnimplementedCourierServiceServer) ResultadoEntrega(context.Context, *Entrega) (*Mensaje, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ResultadoEntrega not implemented")
 }
 func (UnimplementedCourierServiceServer) mustEmbedUnimplementedCourierServiceServer() {}
 
@@ -147,6 +203,78 @@ func _CourierService_Seguimiento_Handler(srv interface{}, ctx context.Context, d
 	return interceptor(ctx, in, info, handler)
 }
 
+func _CourierService_PedirRetail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Mensaje)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CourierServiceServer).PedirRetail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/CourierService/PedirRetail",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CourierServiceServer).PedirRetail(ctx, req.(*Mensaje))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CourierService_PedirPrioritario_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Mensaje)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CourierServiceServer).PedirPrioritario(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/CourierService/PedirPrioritario",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CourierServiceServer).PedirPrioritario(ctx, req.(*Mensaje))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CourierService_PedirNormal_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Mensaje)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CourierServiceServer).PedirNormal(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/CourierService/PedirNormal",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CourierServiceServer).PedirNormal(ctx, req.(*Mensaje))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _CourierService_ResultadoEntrega_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Entrega)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(CourierServiceServer).ResultadoEntrega(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/CourierService/ResultadoEntrega",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(CourierServiceServer).ResultadoEntrega(ctx, req.(*Entrega))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _CourierService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "CourierService",
 	HandlerType: (*CourierServiceServer)(nil),
@@ -162,6 +290,22 @@ var _CourierService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Seguimiento",
 			Handler:    _CourierService_Seguimiento_Handler,
+		},
+		{
+			MethodName: "PedirRetail",
+			Handler:    _CourierService_PedirRetail_Handler,
+		},
+		{
+			MethodName: "PedirPrioritario",
+			Handler:    _CourierService_PedirPrioritario_Handler,
+		},
+		{
+			MethodName: "PedirNormal",
+			Handler:    _CourierService_PedirNormal_Handler,
+		},
+		{
+			MethodName: "ResultadoEntrega",
+			Handler:    _CourierService_ResultadoEntrega_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
